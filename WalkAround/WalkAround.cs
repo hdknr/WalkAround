04cfdd5c4fee381cd95a10ff5a687a041b3a41a1
hdknr <gmail@hdknr.com>
2016-01-08 00:38:38 +0900

    pakage updates

--- WalkAround/WalkAround.cs
+++ WalkAround/WalkAround.cs
@@ -9,18 +9,6 @@ namespace WalkAround
 
 		public App ()
 		{
-			// The root page of your application
-//			MainPage = new ContentPage {
-//				Content = new StackLayout {
-//					VerticalOptions = LayoutOptions.Center,
-//					Children = {
-//						new Label {
-//							XAlign = TextAlignment.Center,
-//							Text = "Welcome to Xamarin Forms!"
-//						}
-//					}
-//				}
-//			};
 			MainPage = new MainContent();
 		}
 
