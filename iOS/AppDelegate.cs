57c0f11b720570d1d2a55c2ee98155ca1ab00ba9
hdknr <gmail@hdknr.com>
2016-01-08 03:09:36 +0900

    iOS Background threading

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -4,17 +4,30 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WalkAround.iOS
 {
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+		public WalkAround.MainContent MainPage { 
+			get { return (WalkAround.MainContent)WalkAround.App.Current.MainPage; } }
+		
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init ();
 
 			LoadApplication (new App ());
+
+
+			new Thread(new ThreadStart(() => {
+				InvokeOnMainThread (() => {
+					MainPage.TimeLabel.Text = DateTime.Now.ToString("T");   
+				});
+			})).Start();
+
 
 			return base.FinishedLaunching (app, options);
 		}
