576389c395786a4206757107f3b3b6d4968e84d2
hdknr <gmail@hdknr.com>
2016-01-12 14:47:03 +0900

    NetTopologySuite.IO.GeoJSON deserializing in PCL

---
+++ WalkAround/Station.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using NetTopologySuite.IO.Converters;
+using NetTopologySuite.Geometries;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WalkAround
+{
+	public class Station
+	{
+		public int id {get;set; }
+		public string name {get;set;}
+
+		[JsonConverter(typeof(GeometryConverter))]
+		public Point location {get;set;}
+
+		[JsonConverter(typeof(GeometryConverter))]
+		public Polygon area {get;set;}
+
+		public static Station Deserialize(string json)
+		{
+			return JsonConvert.DeserializeObject<Station> (json);
+		}
+	}
+}
+
